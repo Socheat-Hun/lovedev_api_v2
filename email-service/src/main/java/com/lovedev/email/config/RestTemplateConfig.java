adeacab044c7e9637c185fd8302a4e36d2afbbc0
Socheat Hun <socheathun23@gmail.com>
2025-12-11 17:01:21 +0900

    Added new common-lib web-starter

--- email-service/src/main/java/com/lovedev/email/config/RestTemplateConfig.java
+++ email-service/src/main/java/com/lovedev/email/config/RestTemplateConfig.java
@@ -1,8 +1,8 @@
 package com.lovedev.email.config;
 
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 import java.time.Duration;
@@ -11,10 +11,12 @@ import java.time.Duration;
 public class RestTemplateConfig {
 
     @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder
-                .setConnectTimeout(Duration.ofSeconds(10))
-                .setReadTimeout(Duration.ofSeconds(10))
-                .build();
+    public RestTemplate restTemplate() {
+        HttpComponentsClientHttpRequestFactory factory =
+                new HttpComponentsClientHttpRequestFactory();
+        factory.setConnectTimeout(10_000); // 10 seconds
+        factory.setReadTimeout(10_000);    // 10 seconds
+
+        return new RestTemplate(factory);
     }
 }
